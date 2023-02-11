be6775058b487fe32550182bcc07f8e63afca717
Liza Parr <lizajparr@gmail.com>
2023-02-11 18:18:53 -0500

    start button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 // click button to start quiz
+var startEl = document.querySelector("#start");
+var clickButton = true;
+
+function startQuiz() {
+    startEl.addEventListener('click', function(){
+        console.log('test');
+        console.dir(clickButton);
+        if(clickButton === true){
+            setTime();
+        }
+        if(clickButton === true){
+            nextQuestion();
+        }
+    })
+
+};
+
+startQuiz();
 
 // start timer
 var timeEl = document.querySelector(".time");
@@ -8,16 +26,20 @@ function setTime() {
     var timer = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = secondsLeft + ' seconds left';
-
         if(secondsLeft === 0) {
             clearInterval(timer);
         }
     }, 1000);
 }
 
-setTime();
+// // initiate first question upon timer starting
+// var switched = element.getAttribute('state');
 
-    // initiate first question upon timer starting
+// function nextQuestion() {
+//     if(switched === 'hidden'){
+        
+//     }
+// }
 
 // clicking an answer reveals next question 
 var choice1El = document.querySelector("#choice1");
@@ -25,7 +47,10 @@ var choice2El = document.querySelector("#choice2");
 var choice3El = document.querySelector("#choice3");
 var choice4El = document.querySelector("#choice4");
 
-choice1El.addEventListener('click');
+// choice1El.addEventListener('click');
+// choice2El.addEventListener('click');
+// choice3El.addEventListener('click');
+// choice4El.addEventListener('click');
 
 // reduce time when answering incorrectly (use boolean values)
 
